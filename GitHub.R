f8384607ef77b63a3ca18c20e4755ac1f190bac8
NicoleDrewitz <nkj1@unak.is>
2025-11-14 09:32:28 +0100

    Added text

---
+++ GitHub.R
@@ -0,0 +1,12 @@
+#install.packages("usethis")
+library(usethis)
+
+usethis::use_git_config(
+  user.name = "NicoleDrewitz",
+  user.email = "nkj1@unak.is"
+)
+
+usethis::create_github_token()
+
+#hello
+#hei
